0684a7433a825b73384e023039991a7bb12cf461
Checho <sergio.avila.astorga@gmail.com>
2023-12-15 21:56:05 -0300

    add bootstrap framework

--- inc/view.php
+++ inc/view.php
@@ -5,37 +5,81 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Votos</title>
     <script src="assets/js/app.js"></script>
-    <link rel="stylesheet" href="assets/css/style.css">k
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <form action="" method="post">
-        <input type="text" name="name" id="name">
-        <input type="text" name="nickname" id="nickname">
-        <input type="text" name="rut" id="rut">
-        <input type="text" name="email" id="email">
-        <select name="state" id="state">
-            <option value="1">Region 1</option>
-            <option value="2">Region 2</option>
-            <option value="3">Region 3</option>
-        </select>
-        <select name="city" id="city">
-            <option value="1">Ciudad 1</option>
-            <option value="2">Ciudad 2</option>
-            <option value="3">Ciudad 3</option>
-        </select>
-        <select name="candidate" id="candidate">
-            <option value="1">Candidato 1</option>
-            <option value="2">Candidato 2</option>
-            <option value="3">Candidato 3</option>
-        </select>
-        checkbox 1
-        <input type="checkbox" name="checkbox1" id="checkbox1">
-        checkbox 2
-        <input type="checkbox" name="checkbox2" id="checkbox2">
-        checkbox 3
-        <input type="checkbox" name="checkbox3" id="checkbox3">
+    <div class="card votes-card">
+        <div class="card-body">
+            <h1 class="fs-2 text-center mb-4">Formulario de Votación</h1>
+            <form action="" method="post">
+                <div class="mb-3">
+                    <label for="fullname" class="form-label">Nombre Completo</label>
+                    <input type="text" class="form-control" name="fullname" id="fullname" required>
+                </div>
+                <div class="mb-3">
+                    <label for="nickname" class="form-label">Alias</label>
+                    <input type="text" class="form-control" name="nickname" id="nickname" required>
+                </div>
+                <div class="mb-3">
+                    <label for="rut" class="form-label">RUT</label>
+                    <input type="text" class="form-control mb-4" name="rut" id="rut" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control mb-4" name="email" id="email" required>
+                </div>
+                <select class="form-select mb-4" name="state" id="state" required>
+                    <option value="">Región</option>
+                    <option value="1">Region 1</option>
+                    <option value="2">Region 2</option>
+                    <option value="3">Region 3</option>
+                </select>
+                <select class="form-select mb-4" name="city" id="city" required>
+                    <option value="">Comuna</option>
+                    <option value="1">Comuna 1</option>
+                    <option value="2">Comuna 2</option>
+                    <option value="3">Comuna 3</option>
+                </select>
+                <select class="form-select mb-4" name="candidate" id="candidate" required>
+                    <option value="">Candidato</option>
+                    <option value="1">Candidato 1</option>
+                    <option value="2">Candidato 2</option>
+                    <option value="3">Candidato 3</option>
+                </select>
+                <p class="fs-5">¿Cómo se enteró de nosotros?</p>
+                <div class="row mb-5">
+                    <div class="col-md-6">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                            <label class="form-check-label" for="flexSwitchCheckDefault">Web</label>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                            <label class="form-check-label" for="flexSwitchCheckDefault">TV</label>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                            <label class="form-check-label" for="flexSwitchCheckDefault">Redes Sociales</label>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                            <label class="form-check-label" for="flexSwitchCheckDefault">Amigo</label>
+                        </div>
+                    </div>
+                </div>
 
-        <input type="submit" value="Votar">
-    </form>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary btn-lg">Votar</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
